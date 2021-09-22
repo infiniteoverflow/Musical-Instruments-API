139e5465bc60e92e356c75fa68658eed62e6b8e3
infiniteoverflow <aswingopinathan1871@gmail.com>
2021-09-22 05:47:19 +0530

    Added support for query parameters

--- bin/server.dart
+++ bin/server.dart
@@ -7,18 +7,47 @@ import 'package:shelf_router/shelf_router.dart';
 
 import 'package:path/path.dart' as path;
 
+class ResponseModel {
+  late List<dynamic> jsonList;
+
+  ResponseModel({required this.jsonList});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'totalRows': jsonList.length,
+      'rows': jsonList.map((e) => e).toList()
+    };
+  }
+}
+
 // Configure routes.
 final _router = Router()
-  ..get('/', _rootHandler)
+  ..get('/', (Request req)=> Response.ok('Homepage'))
+  ..get('/api', _rootHandler)
   ..get('/echo/<message>', _echoHandler);
 
 Future<Response> _rootHandler(Request req) async {
+  var queryParameters = req.requestedUri.queryParameters;
+  print(queryParameters);
+
+  
+
   final targetFile =
     File(path.join(path.dirname(Platform.script.toFilePath()), 'Musical_Instruments_5.json'));
   if (await targetFile.exists()) {
     print('Serving data from $targetFile');
     String fileContent = await targetFile.readAsString();
-    return Response.ok(fileContent);
+    List<dynamic> fileContentList = jsonDecode(fileContent);
+    
+    if(queryParameters.containsKey('offset')) {
+      fileContentList = fileContentList.skip(int.parse(queryParameters['offset']!)).toList();
+    }
+
+    if(queryParameters.containsKey('pageSize')) {
+      fileContentList = fileContentList.take(int.parse(queryParameters['pageSize']!)).toList();
+    }
+
+    return Response.ok(jsonEncode(ResponseModel(jsonList: fileContentList)));
   } else {
     print("$targetFile doesn't exists, stopping");
     return Response.ok(jsonEncode({'Error':'file not found'}));
